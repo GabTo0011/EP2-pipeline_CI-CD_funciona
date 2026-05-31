dbede55138df52e1bd35d17c43264247d75b6e9d
Gabriel Toledo <gab.toledo@duocuc.cl>
2026-05-31 10:41:29 -0400

    test_despliegue_local-pull_GHCR_image-V1.0.0-UPDATE

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!-TEST"', () => {
+      expect(appController.getHello()).toBe('Hello World!-TEST');
     });
   });
 });
